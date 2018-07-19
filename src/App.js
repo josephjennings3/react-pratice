763a7dfc8827a24a36007fd7d894e7edb9a93726
lakazepo <jajenningsiii@gmail.com>
2018-07-19 11:57:36 -0700

    saving before demolishing to include other components as practice

--- src/App.js
+++ src/App.js
@@ -7,34 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Master React</h1>
-        </header>
-        <p className="App-intro">
-          This is the start of mastering React.js
-        </p>
-        <div>
-          <Navbar>
-            <Navbar.Header>
-              <a href="#">React Nav Bar</a>
-            </Navbar.Header>
-            <Nav>
-              <NavItem href="#">
-                Home
-              </NavItem>
-              <NavItem href="#">
-                About
-              </NavItem>
-              <NavItem href="#">
-                Articles
-              </NavItem>
-              <NavItem href="#">
-                Contact
-              </NavItem>
-            </Nav>
-          </Navbar>
-        </div>
+        
       </div>
     );
   }
